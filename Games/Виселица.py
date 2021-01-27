b133a7c118f46fb89b362d60a30e21f152960563
Kate-Pod <75089953+Kate-Pod@users.noreply.github.com>
2021-01-27 23:14:51 +0300

    Update Виселица.py

--- Games/Виселица.py
+++ Games/Виселица.py
@@ -1,6 +1,6 @@
 # Игра Виселица
 def hangman(word):
-    wrong = 0  # сколько неправильных предположений сделано
+    wrong = 0  #сколько неправильных предположений сделано
     stages = ["_____________            ",
               "|            |           ",
               "|            |           ",
@@ -10,17 +10,17 @@ def hangman(word):
               "|                        ",
               ]
     rletters = list(word)
-    board = ["__"] * len(word)
-    win = False
+    board = ["__"] * len(word) #визуал - какие буквы уже угаданы (или ничего: __ __ __)
+    win = False #победил ли уже игрок?
     print("Добро пожаловать на казнь")
     while wrong < len(stages):
-        print("\n")
+        print("\n") #для лучшего отображения
         msg = "Введите букву: "
         char = input(msg)
-        if char in rletters:
-            cind = rletters.index(char)
-            board[cind] = char
-            rletters[cind] = '$'
+        if char in rletters:            #если значение буквы-догадки содержится в загаданном слове
+            cind = rletters.index(char) #находим индекс этой буквы в загаданном слове
+            board[cind] = char          #обновляем board-вывод визуального отображения
+            rletters[cind] = '$'        #заменяем правильно угаданный символ в слове знаком $ (чтобы в случае повторяющихся букв, алгоритм не выводил один и тот же индекс)
         else:
             wrong += 1
         print((" ".join(board)))
