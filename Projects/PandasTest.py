3b34bb720eed0ee9761bec44e5b6f015b7ad4ea0
Kate-Pod <75089953+Kate-Pod@users.noreply.github.com>
2021-03-09 23:10:48 +0300

    Update PandasTest.py

--- Projects/PandasTest.py
+++ Projects/PandasTest.py
@@ -10,6 +10,16 @@ students_with_names.index=['Cercei','Tywin','Joffrey','Ilyn Payne','Gregor'] #д
 student_performance_with_names.loc =['Cercei', 'Joffrey'],['gender', 'writing score']] #выберем данные по индексам и столбцам
 
 
-У какой доли студентов из датасэта в колонке lunch указано free/reduced?
+#У какой доли студентов из датасэта в колонке lunch указано free/reduced?
 students.lunch.loc[students.lunch=='free/reduced'].count()/students.lunch.count()
 
+#Сравним оценки студентов с урезанным ланчем и стандартным по 3 дисциплинам 
+students[students.lunch=='standard'].describe()-students[students.lunch=='free/reduced'].describe()
+
+#Переименуем названия столбцов для удобства работы с ними
+students=students.rename(columns={'parental level of education': 'parental_level_of_education',
+                                            'test preparation course': 'test_preparation_course',
+                                            'math score': 'math_score',
+                                            'reading score': 'reading_score',
+                                            'writing score': 'writing_score'})
+students.query("writing_score>74")
