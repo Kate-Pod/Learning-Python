8a094b5eeae8ac63434677262cceb923319eca27
Kate-Pod <75089953+Kate-Pod@users.noreply.github.com>
2021-03-14 23:30:25 +0300

    Create Visualization.py

---
+++ Projects/Visualization.py
@@ -0,0 +1,30 @@
+# отключим предупреждения Anaconda
+import warnings
+warnings.simplefilter('ignore')
+
+# будем отображать графики прямо в jupyter'e
+%matplotlib inline
+import seaborn as sns
+import matplotlib.pyplot as plt
+#графики в svg выглядят более четкими
+%config InlineBackend.figure_format = 'svg' 
+
+#увеличим дефолтный размер графиков
+from pylab import rcParams
+rcParams['figure.figsize'] = 8, 5
+import pandas as pd
+
+df=pd.read_csv('C:/.../Video_Games_Sales.csv')
+df.info()
+df = df.dropna()
+df['User_Score'] = df.User_Score.astype('float64')
+df['Year_of_Release'] = df.Year_of_Release.astype('int64')
+df['User_Count'] = df.User_Count.astype('int64')
+df['Critic_Count'] = df.Critic_Count.astype('int64')
+
+print(df.shape) #в таблице 6825 объектов и 16 признаков для них
+useful_cols = ['Name', 'Platform', 'Year_of_Release', 'Genre', 
+               'Global_Sales', 'Critic_Score', 'Critic_Count',
+               'User_Score', 'User_Count', 'Rating'
+              ]
+df[useful_cols].head()
